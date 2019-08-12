fd0f126fe5415392a7cb769a3e7b6da4d9086bed
disha28 <disrastogi7@gmail.com>
2019-08-12 10:26:23 +0530

    hourglass sum

---
+++ hourglass sum.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int i,j,a[6][6],m,n,maxSum = -36985247;
+for(i=0;i<6;i++)
+{
+    for(j=0;j<6;j++)
+    {
+        cin>>a[i][j];
+    }
+}
+
+for(i=0;i<6;i++)
+{
+    for(j=0;j<6;j++)
+    {
+       if(j+2<6&&i+2<6)
+       {
+           int sum= a[i][j]+a[i][j+1]+a[i][j+2]+a[i+1][j+1]+a[i+2][j]+a[i+2][j+1]+a[i+2][j+2];
+           if(sum > maxSum)maxSum = sum;
+            
+       }
+        
+    }
+}
+cout<<maxSum<<"\n";	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
