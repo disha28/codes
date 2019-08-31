cfddd21a0596788314856046cf719a171e13d191
disha28 <disrastogi7@gmail.com>
2019-08-31 17:54:01 +0530

    captilization

---
+++ capitalization.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string s;
+//	char ch;
+	 
+	cin>>s;
+	if(islower(s[0]))
+	s[0]=toupper(s[0]);
+	cout<<s;
+	
+
+       
+	
+	
+	return 0;
+}
