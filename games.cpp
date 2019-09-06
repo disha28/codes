68c4df5f5ca7ec5e15ee9e0f0ecf8561ae553c19
disha28 <disrastogi7@gmail.com>
2019-09-07 00:31:29 +0530

    game

---
+++ games.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int n,i,c=0,j;
+	cin>>n;
+	int a[n],b[n];
+	for(i=0;i<n;i++)
+	{
+	    cin>>a[i]>>b[i];
+	}
+	for(i=0;i<n;i++)
+	{
+	    for(j=0;j<n;j++)
+	    {
+	        if(a[i]==b[j])
+	        c++;
+	    }
+	}
+	cout<<c;
+	
+	return 0;
+}
