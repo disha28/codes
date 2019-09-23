afd384f5f55b9b0efd9d35ac02104442bb259376
disha28 <disrastogi7@gmail.com>
2019-09-24 00:40:26 +0530

    sum of n-1 natural nos

---
+++ hanshakes.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int t,n,i,j,c=0;
+cin>>t;
+while(t>0)
+{
+    cin>>n;
+    cout<<(n*(n-1)/2)<<"\n";
+  
+    t--;
+}
+
+	return 0;
+}
