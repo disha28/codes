dbc623ac574f5fbb149423d2cb9663c57646ed8a
disha28 <disrastogi7@gmail.com>
2019-09-29 02:40:35 +0530

    maths

---
+++ maths.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define m 1000000007
+
+int main() {
+    
+ long long  int t,i;
+   cin>>t;
+   while(t--)
+   {
+      long long int n;
+       cin>>n;
+       long long int s=0;
+     //  for(i=1;i<=n;i++)
+    
+           
+           cout<<((n%m)*(n%m))%m<<endl;
+         //  x=x+s;
+           
+       
+      // a=x%m;
+       //cout<<s<<endl;
+       
+   }
+    
+    
+    return 0;
+}
