a086d45a4a9778634d187c434ee2736e6af65e1b
disha28 <disrastogi7@gmail.com>
2020-03-08 16:45:36 +0530

    matrix exponentiation

---
+++ matrix exponentiation using naive approach.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for(int i=1;i<=n;i++)
+#define N 101
+int ar[N][N],I[N][N];
+void mul(int A[][N],int B[][N],int dim)
+{
+    int res[dim +1][dim+1];
+    REP(i,dim) 
+    {
+        REP(j,dim)
+    {
+        res[i][j]=0;
+        REP(k,dim) res[i][j]+=A[i][k]*B[k][j];
+    }
+    
+    }
+    REP(i,dim) REP(j,dim)
+    A[i][j]=res[i][j];
+    
+}
+void power(int A[][N],int dim,int n)
+{
+    REP(i,dim) REP(j,dim)
+    {
+        if(i==j)
+        I[i][j]=1;
+        else
+        I[i][j]=0;
+    }
+    REP(i,n)
+   mul(I,A,dim);
+    REP(i,dim) REP(j,dim)
+    A[i][j]=I[i][j];
+}
+void printMat(int A[][N],int dim)
+{
+    REP(i,dim)
+    {
+        REP(j,dim)
+        
+            cout<<A[i][j]<<" ";
+            cout<<endl;
+        
+    }
+}
+
+int main() {
+    int t,dim,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>dim>>n;
+        REP(i,dim) REP(j,dim) cin>>ar[i][j];
+        
+        
+        
+        power(ar,dim,n);
+        printMat(ar,dim);
+    }
+	return 0;
+}
