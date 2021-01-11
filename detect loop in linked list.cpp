6544fad713a6e85b70081f1ad1b8b0546bc2a8fd
disha28 <disrastogi7@gmail.com>
2021-01-11 16:18:09 +0530

    detect loop in linked list using floyd loop detection

---
+++ detect loop in linked list.cpp
@@ -0,0 +1,17 @@
+
+bool detectLoop(Node* head)
+{
+    // your code here
+    if(head==NULL||head->next==NULL)
+    return false;
+    struct Node* fast=head;
+    struct Node* slow=head;
+    while(fast->next!=NULL &&fast->next->next!=NULL)
+    {
+        fast=fast->next->next;
+        slow=slow->next;
+        if(fast==slow)
+        return true;
+    }
+    return false;
+}
